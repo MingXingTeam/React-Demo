f07ab9828eb389eb56f4f900fa963dfd477fb5a5
maomao <813106093@qq.com>
2016-01-01 16:42:29 +0800

    init commit

---
+++ jsx/index.jsx
@@ -0,0 +1,49 @@
+/*
+TODO APP
+*/
+var ToDoList = React.createClass({
+	
+	render:function(){
+		var createItem = function(item){
+			return <li key={item.id}>{item.text}</li>
+		}
+		return (
+			<ul>
+				{
+					this.props.items.map(creatItem)
+				}
+			</ul>
+		)
+	}
+})
+
+var ToDoApp = React.createClass({
+	getInitialState:function(){
+		return {items:[],text:""}
+	},
+	onChange:function(e){
+		this.setState({
+			text:e.target.value
+		});
+	},
+	handleSubmit:function(e){
+		e.preventDefault();
+		var nextItems = this.state.items.concat([{text:this.state.text,id:Date().now()}]);
+		var nextText = "";
+		this.setState({
+			items:nextItems,text:nextText
+		})
+	},
+	render:function(){
+			return (
+					<div>
+						<h3>TODO APP</h3>	
+						<ToDoList items={this.state.items} />
+						<form onSubmit={this.handleSubmit}>
+							<input onChange={this.onChange} value={this.state.text}>
+							<button>{'add #'+this.state.items.length + 1}</button>
+						</form>
+					</div>
+				)
+	}
+})
